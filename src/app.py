43f7d3ec2e740ed4558cb8b07b2bd71c5f49b246
Tuukka Tuomikoski <tuukka.tuomikoski@gmail.com>
2021-11-23 23:58:41 +0200

    edited tests

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ def main():
     caller = Caller()
     print("Getting last 100 created tokens on the Ethereum blockchain")
 
-    contracts = caller.get_contracts_created_recently(20)
+    contracts = caller.get_contracts_created_recently(2,hex(13673101))
     for contract in contracts:
         print(contract.name,contract.address,contract.creator_address)
 
