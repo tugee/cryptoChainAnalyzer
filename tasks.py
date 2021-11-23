aae55c9094cbc7618d85765b6c3629960f54ace2
Tuukka Tuomikoski <tuukka.tuomikoski@gmail.com>
2021-11-24 00:00:11 +0200

    tasks edited

--- tasks.py
+++ tasks.py
@@ -8,7 +8,7 @@ def test(ctx):
     ctx.run("pytest src")
 @task
 def coverage(ctx):
-    ctx.run("coverage run --branch -m pytest")
+    ctx.run("coverage run --branch -m pytest src")
 
 @task(coverage)
 def coverage_report(ctx):
